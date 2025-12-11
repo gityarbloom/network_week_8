b8c45c1261bf259f7ad44f0e305250d777956aba
Israel-Yarbloom <i0504170922@gmail.com>
2025-12-11 12:55:40 +0200

    test week 8

--- main.py
+++ main.py
@@ -28,6 +28,43 @@ def get_mask_address():
         counter += 1
     return address
 
+# from core.utils import *
+
+
+# def net_address_by_mask():
+#     my_data = {}
+#     mask = input("\nPLEASE ENTER A S.B. MASK \n--(10 NUMBERS BETWEEN 0 TO 1 ONLY, ONES FIRST AND THE ZEROS AT THE END): ")
+#     while not validate_mask(mask):
+#         mask = input("\nPLEASE ENTER A S.B. MASK \n--(10 NUMBERS BETWEEN 0 TO 1 ONLY, ONES FIRST AND THE ZEROS AT THE END): ")
+#     my_data["Binary Mask Input: "] = mask
+
+#     dec_ip_number = int(input("\nPLEASE ENTER A NUMBER BETWEEN 0 AND 1023: "))
+#     while not validate_dec(dec_ip_number):
+#         dec_ip_number = int(input("\nPLEASE ENTER A NUMBER BETWEEN 0 AND 1023: "))
+#     my_data["Decimal Input: "] = dec_ip_number
+#     my_data["Input in Binary: "] = decimal_to_binary(dec_ip_number)
+
+#     dec_ip_number = decimal_to_binary(dec_ip_number)
+#     bin_ip_address = split_by_mask(mask, dec_ip_number)
+#     my_data["Left Part (Binary): "] = bin_ip_address[0]
+#     my_data["Right Part (Binary): "] = bin_ip_address[1]
+#     my_data["Resultin Decimal: "] = dec_ip_by_mask(bin_ip_address)
+#     return my_data
+
+# def update_data(my_data: dict):
+#     with open("bit_mask_result_211846894.txt", "a") as data:
+#         data.write("\n" + "*" * 15)
+#         data.write("\n")
+#         for key, value in my_data.items():
+#             new_row = str(key) + str(value)
+#             data.write(new_row)
+#             data.write("\n")
+#         return
+
+# if __name__ == "__main__":
+#     my_data = net_address_by_mask()
+#     update_data(my_data)
+
 if __name__ == "__main__":
     get_address()
     get_mask_address()
